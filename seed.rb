a4ccf6bfd97a4ec70a343e0d90307e46771264dd
Joshua Bell <josh@joshuabell.io>
2018-02-08 17:07:28 -0800

    correct boundary check in dev seed script

--- seed.rb
+++ seed.rb
@@ -2,6 +2,6 @@ prng = Random.new
 
 1000.times do |n|
   task = Task.create(title: 'a task', priority: prng.rand(10))
-  next if n == 1
+  next if task.id == 1
   task.tasks << Task.find(task.id - 1)
 end
